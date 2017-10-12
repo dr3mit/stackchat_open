fa1e9fc06ad92d4902b5bee3e3d86ccc6d99dc9b
Kate Humphrey <kate@fullstackacademy.com>
2017-10-12 12:42:05 -0400

    update redux-logger

--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,7 @@ import {
   applyMiddleware,
   combineReducers
 } from 'redux';
-import createLogger from 'redux-logger';
+import loggingMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -33,7 +33,7 @@ const store = createStore(
   reducer,
   composeWithDevTools(applyMiddleware(
     thunkMiddleware,
-    createLogger()
+    loggingMiddleware
   ))
 );
 
